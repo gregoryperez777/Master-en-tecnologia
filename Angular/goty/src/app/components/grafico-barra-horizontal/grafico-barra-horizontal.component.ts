ab273e8f65ccddbbec48053da82ff90406e69a27
Gregory Perez <gregoryperezfigueredo@gmail.com>
2020-02-17 15:02:31 -0400

    first commit of the Angular facilities course, initialization and start of the game of the year project

---
+++ Angular/goty/src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grafico-barra-horizontal',
+  templateUrl: './grafico-barra-horizontal.component.html',
+  styleUrls: ['./grafico-barra-horizontal.component.css']
+})
+export class GraficoBarraHorizontalComponent {
+
+  results: any[] = [
+    {
+      "name": "Juego 1",
+      "value": 20
+    },
+    {
+      "name": "Juego 2",
+      "value": 15
+    },
+    {
+      "name": "Juego 3",
+      "value": 25
+    },
+    {
+      "name": "Juego 4",
+      "value": 23
+    }
+  ];
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = true;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Votos';
+  showYAxisLabel = true;
+  yAxisLabel = 'Juegos';
+
+  colorScheme = 'nightLights';
+
+  constructor() {
+  }
+
+  onSelect(event) {
+    console.log(event);
+  }
+
+}
